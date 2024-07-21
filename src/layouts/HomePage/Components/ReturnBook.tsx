076dce3c6414704991c8ffb0b345a9da0eec20c5
Anurag Pandey <anurag2992000@gmail.com>
2024-07-21 16:38:58 +0530

    API Calling from the Frontend.

--- src/layouts/HomePage/Components/ReturnBook.tsx
+++ src/layouts/HomePage/Components/ReturnBook.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import BookModel from "../../../models/BookModel";
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
   return (
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
       <div className="text-center">
-        <img
-          src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
-          width="151"
-          height="233"
-          alt="book"
-        />
+        {props.book.img ? (
+          <img src={props.book.img} width="151" height="233" alt="book" />
+        ) : (
+          <img
+            src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
+            width="151"
+            height="233"
+            alt="book"
+          />
+        )}
         <h6 className="mt-2">Book</h6>
         <p>BookNest</p>
         <a className="btn main-color text-white" href="#">
