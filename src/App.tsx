a1028718560f0f71a80fbef003c736bdd830302c
Anurag Pandey <anurag2992000@gmail.com>
2024-07-22 17:19:53 +0530

    Added routing to the parent level component

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <Route path="/" exact>
+        <Redirect to="/home" />
+      </Route>
+      <Route path="/home">
+        <HomePage />
+      </Route>
+      <Route path="/search">
+        <SearchBooksPage />
+      </Route>
       <Footer />
     </div>
   );
