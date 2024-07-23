3918d451ac8f4db44acf044bd539846da91ea57f
Anurag Pandey <anurag2992000@gmail.com>
2024-07-23 14:59:17 +0530

    Checkout Page completed. Routing to be done.

---
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import { useParams } from "react-router-dom";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StarsReview } from "../Utils/StarsReview";
+import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
+
+export const BookCheckoutPage = () => {
+  const [book, setBook] = useState<BookModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  const { bookId } = useParams<{ bookId: string }>(); //  returns an object with string values.
+
+  //console.log(bookId);
+
+  //   or you can use =>>
+  //   const bookId = (window.location.pathname).split('/')[2]
+  //   window.location.pathname => /api/books/123
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const baseurl = `http://localhost:8080/api/books/${bookId}`;
+
+      const response = await fetch(baseurl);
+
+      if (!response.ok) throw new Error("Something went wrong!");
+
+      const responseJson = await response.json();
+
+      const loadedBook: BookModel = {
+        id: responseJson.id,
+        title: responseJson.title,
+        author: responseJson.author,
+        description: responseJson.description,
+        copies: responseJson.copies,
+        copiesAvailable: responseJson.copiesAvailable,
+        category: responseJson.category,
+        img: responseJson.img,
+      };
+
+      setBook(loadedBook);
+      setIsLoading(false);
+    };
+    fetchBooks().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return (
+    // For desktop
+
+    <div>
+      <div className="container d-none d-lg-block">
+        <div className="row mt-5">
+          <div className="col-sm-2 col-md-2">
+            {book?.img ? (
+              <img src={book?.img} width="226" height="349" alt="Book" />
+            ) : (
+              <img
+                src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                width="226"
+                height="349"
+                alt="Book"
+              />
+            )}
+          </div>
+
+          <div className="col-4 col-md-4 container">
+            <div className="ml-2">
+              <h2>{book?.title}</h2>
+              <h5 className="text-primary">{book?.author}</h5>
+              <p className="lead">{book?.description}</p>
+              <StarsReview rating={2} size={32} />
+            </div>
+          </div>
+          <CheckoutAndReviewBox book={book} mobile={false} />
+        </div>
+        <hr />
+      </div>
+
+      {/* Mobile Version */}
+      <div className="container d-lg-none mt-5">
+        <div className="d-flex justify-content-center align-items-center">
+          {book?.img ? (
+            <img src={book?.img} width="226" height="349" alt="Book" />
+          ) : (
+            <img
+              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+              width="226"
+              height="349"
+              alt="Book"
+            />
+          )}
+        </div>
+        <div className="mt-4">
+          <div className="ml-2">
+            <h2>{book?.title}</h2>
+            <h5 className="text-primary">{book?.author}</h5>
+            <p className="lead">{book?.description}</p>
+            <StarsReview rating={4} size={32} />
+          </div>
+        </div>
+        <CheckoutAndReviewBox book={book} mobile={false} />
+        <hr />
+      </div>
+    </div>
+  );
+};
